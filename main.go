2f9aa72a929147378f1f2e74d924ee29399f3496
Isaiah-Mwinga <motongori@marketforce360.com>
2023-10-19 20:47:01 +0300

    Go learn

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 100
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We currently have %v tickets remaining\n", remainingTickets)
 }
