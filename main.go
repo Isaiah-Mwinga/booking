d1e4f9ae1b5622393c7213dab1458fbb04330a63
Isaiah-Mwinga <motongori@marketforce360.com>
2023-11-05 22:56:39 +0300

    booking

--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 100
-	var remainingTickets = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We currently have %v tickets remaining\n", remainingTickets)
+	fmt.Printf("We currently have %v tickets available!\n", conferenceTickets)
 }
