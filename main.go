4c53e52412bdb3346630d78e3869d29c8fc97154
Isaiah-Mwinga <motongori@marketforce360.com>
2023-11-09 20:25:33 +0300

    commit

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 100
-
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We currently have %v tickets available!\n", conferenceTickets)
+	fmt.Println("Hello, World!")
 }
