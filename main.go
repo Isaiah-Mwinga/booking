b2f01539ba9bb7b139ca41b3b6d861aefcc0e1f8
Isaiah-Mwinga <motongori@marketforce360.com>
2023-11-09 20:40:05 +0300

    Go API

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"errors"
+)
+
+type book struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Quality string `json:"quality"`
+	
+}
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+
 
 func main() {
 	fmt.Println("Hello, World!")
