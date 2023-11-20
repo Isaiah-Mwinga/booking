7c384dfb0e6d82315e1c38207037fe36428cf696
Isaiah-Mwinga <motongori@marketforce360.com>
2023-11-20 21:14:50 +0300

    webserver

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
 	fmt.Println("Hello, World!")
 }
