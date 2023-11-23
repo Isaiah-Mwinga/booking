e107e0cf5c9c0ed975d508871beecfd9390b5c8e
Isaiah-Mwinga <motongori@marketforce360.com>
2023-11-23 20:12:40 +0300

    webserver

--- server/main.go
+++ server/main.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing form: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	age := r.FormValue("age")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Age = %s\n", age)
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
