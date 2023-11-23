8ab8ea7ecf638c37a7f6c82bf9ea2daf8eb2886e
Isaiah-Mwinga <motongori@marketforce360.com>
2023-11-23 20:16:17 +0300

    webserver

--- server/main.go
+++ server/main.go
@@ -33,5 +33,10 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
